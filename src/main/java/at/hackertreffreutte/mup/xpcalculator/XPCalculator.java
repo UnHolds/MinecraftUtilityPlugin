b335a2bde824f4f6fb378f0fb9592a8b48d83847
Thomas Klotz <thomas.klotz98@gmail.com>
2023-05-28 15:32:08 +0200

    refactor XPCalculator

--- src/main/java/at/hackertreffreutte/mup/xpcalculator/XPCalculator.java
+++ src/main/java/at/hackertreffreutte/mup/xpcalculator/XPCalculator.java
@@ -2,39 +2,35 @@ package at.hackertreffreutte.mup.xpcalculator;
 
 public class XPCalculator {
 
-
-    //those are some serious magic numbers
-    //https://minecraft.gamepedia.com/Experience#Leveling_up
-    public static int levelToExp(int level){
-
-        if(level <= 15){
-            return (int) Math.round(Math.pow(level,2) + 6 * level);
-        } else if(level <= 31){
-            return (int) Math.round( 2.5 * Math.pow(level,2) - 40.5 * level + 360);
-        } else {
-            return (int) Math.round( 4.5 * Math.pow(level,2) - 162.5 * level + 2220);
-        }
-
+    private XPCalculator() {
     }
 
     //those are some serious magic numbers
     //https://minecraft.gamepedia.com/Experience#Leveling_up
-    public static int expToLevel(int exp){
+    public static int levelToExp(int level) {
 
-        int level = 0;
-        if(exp <= 351){
-            level = (int) Math.floor(-3 + Math.sqrt(exp + 9));
-        }else if(exp <= 1623){
-            level = (int) Math.floor(8.1 + 0.1 * Math.sqrt(40*exp - 7839));
-        }else{
-            level = (int) Math.floor(18.0556 + 0.05555555 * Math.sqrt(72 * exp - 54215));
+        if (level <= 15) {
+            return (int) Math.round(Math.pow(level, 2) + 6 * level);
+        } else if (level <= 31) {
+            return (int) Math.round(2.5 * Math.pow(level, 2) - 40.5 * level + 360);
+        } else {
+            return (int) Math.round(4.5 * Math.pow(level, 2) - 162.5 * level + 2220);
+        }
+    }
+
+    //those are some serious magic numbers
+    //https://minecraft.gamepedia.com/Experience#Leveling_up
+    public static int expToLevel(int exp) {
+        int level;
+
+        if (exp <= 351) {
+            level = (int) Math.floor(-3 + Math.sqrt(exp + 9.0));
+        } else if (exp <= 1623) {
+            level = (int) Math.floor(8.1 + 0.1 * Math.sqrt(40 * exp - 7839.0));
+        } else {
+            level = (int) Math.floor(18.0556 + 0.05555555 * Math.sqrt(72 * exp - 54215.0));
         }
 
-        if(level < 0){
-            level = 0;
-        }
-
-        return level;
-
+        return Math.max(level, 0);
     }
 }
