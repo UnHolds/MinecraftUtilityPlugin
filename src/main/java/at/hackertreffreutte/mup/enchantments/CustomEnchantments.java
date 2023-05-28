1d6594300e9acf509ebe30bf0203396551466257
Thomas Klotz <thomas.klotz98@gmail.com>
2023-05-28 15:54:19 +0200

    refactor CustomEnchantments

--- src/main/java/at/hackertreffreutte/mup/enchantments/CustomEnchantments.java
+++ src/main/java/at/hackertreffreutte/mup/enchantments/CustomEnchantments.java
@@ -1,39 +1,25 @@
 package at.hackertreffreutte.mup.enchantments;
 
 import org.bukkit.enchantments.Enchantment;
+
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class CustomEnchantments {
 
-
-
-    public static boolean register(Enchantment enchantment) {
+    public static void register(Enchantment enchantment) {
         boolean registered = Arrays.stream(Enchantment.values()).collect(Collectors.toList()).contains(enchantment);
 
-        if(registered){
-            //enchantment already registered
-        }else{
-            try{
+        if (!registered) {
+            try {
                 Field f = Enchantment.class.getDeclaredField("acceptingNew");
                 f.setAccessible(true);
                 f.set(null, true);
                 Enchantment.registerEnchantment(enchantment);
-            }catch (Exception e){
-                registered = false;
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-
-            if(registered){
-                //send message to console
-                //TODO send message to console
-
-            }
         }
-
-        return registered;
     }
-
-
 }
