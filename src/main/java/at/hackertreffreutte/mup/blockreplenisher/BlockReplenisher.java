870563da815519d15c82db163b1eaf652cf066e1
Thomas Klotz <thomas.klotz98@gmail.com>
2023-05-28 15:48:13 +0200

    refactor BlockReplenisher

--- src/main/java/at/hackertreffreutte/mup/blockreplenisher/BlockReplenisher.java
+++ src/main/java/at/hackertreffreutte/mup/blockreplenisher/BlockReplenisher.java
@@ -13,24 +13,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlockReplenisher implements Listener {
 
-
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event){
 
         //placed last item
         if(event.getItemInHand().getAmount() == 1){
 
-
             //TODO
             //workaround fix later otherwise shovel will be removed when making a path
             if(event.getItemInHand().getMaxStackSize() == 1){
                 return;
             }
-
-            final PlayerInventory inv = event.getPlayer().getInventory();
-
-            //get material of placed item
-            Material placedMaterial = event.getItemInHand().getType();
 
             //remove placed item
             if(event.getHand() == EquipmentSlot.HAND){
@@ -39,29 +32,25 @@ public class BlockReplenisher implements Listener {
                 event.getPlayer().getEquipment().getItemInOffHand().setAmount(0);
             }
 
+            final PlayerInventory inventory = event.getPlayer().getInventory();
+            Material placedMaterial = event.getItemInHand().getType();
 
-            for(int i= 0; i < inv.getContents().length; i++){
-                final ItemStack item = inv.getItem(i);
+            for(ItemStack item : inventory.getContents()){
 
-                if(item != null){
-                    if(item.getType().equals(placedMaterial)){
-                        //found same item like that that was placed
+                if(item != null && (item.getType().equals(placedMaterial))){
+                    //found same item like that that was placed
 
-                        //replenish the item (move the item from the inventory to the main hand)
-                        Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(Main.class), new Runnable() {
-                            public void run() {
-                                if(event.getHand() == EquipmentSlot.HAND){
-                                    inv.setItemInMainHand(item.clone());
-                                    item.setAmount(0);
-                                }else if(event.getHand() == EquipmentSlot.OFF_HAND){
-                                    inv.setItemInOffHand(item.clone());
-                                    item.setAmount(0);
-                                }
-                            }
-                        },1L);
-
-                        break;
-                    }
+                    //replenish the item (move the item from the inventory to the main hand)
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(Main.class), () -> {
+                        if(event.getHand() == EquipmentSlot.HAND){
+                            inventory.setItemInMainHand(item.clone());
+                            item.setAmount(0);
+                        }else if(event.getHand() == EquipmentSlot.OFF_HAND){
+                            inventory.setItemInOffHand(item.clone());
+                            item.setAmount(0);
+                        }
+                    },1L);
+                    break;
                 }
             }
         }
