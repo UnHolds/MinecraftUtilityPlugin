d7633cf5173ef3b14426eaefcbfbd3e2df160ec5
Thomas Klotz <thomas.klotz98@gmail.com>
2023-05-28 16:01:56 +0200

    refactor ToolBreakListener

--- src/main/java/at/hackertreffreutte/mup/toolreplenisher/ToolBreakListener.java
+++ src/main/java/at/hackertreffreutte/mup/toolreplenisher/ToolBreakListener.java
@@ -11,66 +11,49 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ToolBreakListener implements Listener {
 
-
-    public boolean isReplenishAble(ItemStack item){
+     public boolean isReplenishAble(ItemStack item){
         switch (item.getType()){
-            //Pickaxes
+
             case DIAMOND_PICKAXE:
             case GOLDEN_PICKAXE:
             case IRON_PICKAXE:
             case STONE_PICKAXE:
             case WOODEN_PICKAXE:
 
-            // shovel
             case DIAMOND_SHOVEL:
             case GOLDEN_SHOVEL:
             case IRON_SHOVEL:
             case STONE_SHOVEL:
             case WOODEN_SHOVEL:
 
-            //axe
             case DIAMOND_AXE:
             case GOLDEN_AXE:
             case IRON_AXE:
             case STONE_AXE:
             case WOODEN_AXE:
 
-             //sword
             case DIAMOND_SWORD:
             case GOLDEN_SWORD:
             case IRON_SWORD:
             case STONE_SWORD:
             case WOODEN_SWORD:
 
-             //hoe
             case DIAMOND_HOE:
             case GOLDEN_HOE:
             case IRON_HOE:
             case STONE_HOE:
             case WOODEN_HOE:
 
-            //shield
             case SHIELD:
-
-             //scissors / shears
             case SHEARS:
-
-             //bow
             case BOW:
-
-            //crossbow
             case CROSSBOW:
-
-             //fishing rod
             case FISHING_ROD:
-
                 return true;
 
-            default:
-                return false;
+            default: return false;
         }
     }
-
 
     @EventHandler
     public void onPlayerToolBreakEvent(PlayerItemBreakEvent event){
@@ -78,34 +61,26 @@ public class ToolBreakListener implements Listener {
         if(isReplenishAble(event.getBrokenItem())){
 
             final PlayerInventory playerInventory = event.getPlayer().getInventory();
-            for(int i = 0; i < playerInventory.getContents().length; i++){
+            for(ItemStack item : playerInventory){
 
-                final ItemStack item = playerInventory.getItem(i);
-                if(item != null){
-                    if(!item.equals(event.getBrokenItem()) && item.getType().equals(event.getBrokenItem().getType())){
+                if(isValidItem(event, item)){
 
-                        //replace the item
-                        Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(Main.class), new Runnable() {
-                            public void run(){
-                                if(event.getBrokenItem().getType().equals(event.getPlayer().getEquipment().getItemInMainHand().getType())){
-                                    playerInventory.setItemInMainHand(item.clone());
-                                    item.setAmount(0);
-                                }else if(event.getBrokenItem().getType().equals(event.getPlayer().getEquipment().getItemInOffHand().getType())){
-                                    playerInventory.setItemInOffHand(item.clone());
-                                item.setAmount(0);
-                            }
-
-                            }
-                        },1L);
-
-                        break;
-                    }
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(Main.class), () -> {
+                        if(event.getBrokenItem().getType().equals(event.getPlayer().getEquipment().getItemInMainHand().getType())){
+                            playerInventory.setItemInMainHand(item.clone());
+                            item.setAmount(0);
+                        }else if(event.getBrokenItem().getType().equals(event.getPlayer().getEquipment().getItemInOffHand().getType())){
+                            playerInventory.setItemInOffHand(item.clone());
+                            item.setAmount(0);
+                        }
+                    },1L);
+                    break;
                 }
             }
-
-        }else{
-            return;
         }
+    }
 
+    private static boolean isValidItem(PlayerItemBreakEvent event, ItemStack item) {
+        return item != null && (!item.equals(event.getBrokenItem()) && item.getType().equals(event.getBrokenItem().getType()));
     }
 }
